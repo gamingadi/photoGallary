e709fe354c0df21b5d9e1242546e4dd29851f21c
Aditya <ankushkaditya10@gmail.com>
2020-07-23 15:43:51 +0530

    add mongo-store

--- server.js
+++ server.js
@@ -11,8 +11,17 @@ const session=require("express-session");
 const imageModel=require("./data/img-model")
 const profile=require("./route/profile")
 const bcrypt = require('bcrypt')
+const MongoStore = require('connect-mongo')(session);
+const connection= mongoose.connect(`mongodb+srv://aditya:${process.env.API_KEY}@gallary.fbhcg.mongodb.net/gallary?retryWrites=true&w=majority`,
+ { useNewUrlParser: true,
+    useUnifiedTopology:true,
+    useCreateIndex:true,
+    useFindAndModify:false
+
+}).catch(error => console.log("database disconnected"));
 
 const sess={
+    store:new MongoStore({mongooseConnection:mongoose.connection,autoRemove:'interval',autoRemoveInterval:10}),
     name:"photo",
     secret:process.env.SECRET,
     resave:true,
@@ -26,13 +35,6 @@ if (app.get('env') === 'production') {
   }
   app.use(session(sess))
 app.use(express.static("upload"))
-mongoose.connect(`mongodb+srv://aditya:${process.env.API_KEY}@gallary.fbhcg.mongodb.net/gallary?retryWrites=true&w=majority`,
- { useNewUrlParser: true,
-    useUnifiedTopology:true,
-    useCreateIndex:true,
-    useFindAndModify:false
-
-}).catch(error => console.log("database disconnected"));
   app.use(bodyParser.urlencoded({extended:true}))
    
     app.set("view engine","ejs")
@@ -44,70 +46,69 @@ mongoose.connect(`mongodb+srv://aditya:${process.env.API_KEY}@gallary.fbhcg.mong
 
 
 app.get("/",function(req,res){
- 
    res.render("login",{data:data,status:""})
 });
-const isconnect = (req,res,next)=>{
-    mongoose.connect('mongodb+srv://aditya:@gallary.fbhcg.mongodb.net/gallary?retryWrites=true&w=majority', { useNewUrlParser: true,useUnifiedTopology:true,useCreateIndex:true }).
-  catch(error => {
-        res.render("login",{data:data,status:"database not connected"})
-    })
-    next()
-    return
-} 
-app.post("/",isconnect,async function(req,res){
-    req.session.email=req.body.email
-    const hash=await bcrypt.hash(req.body.password,10)
-        
-    if(req.body.btn=="register"){
-        //Creating User document
-        const User =new auth({
-            username:req.body.name,
-            email:req.body.email,
-            password:hash
-        })
-        //Save data into MongoDB
-        User.save( function(err,result){
-            if(err){
-                res.render("login",{status:"something wrong"}) 
-            }else{
-                //Here,email are also stores in file collections 
-                const data=new imageModel({
-                    email:result.email,
-                    profile:"profileimage/woman-1807533_1920.jpg"
-                })
-                data.save((err,result)=>{
-                    res.render("login",{status:"Register completed"})
-                })                
-            }  
-        })
-    }
-    if(req.body.btn=="Login"){
-     
-       auth.findOne({email:req.body.email},function(err,result){    
-        if(err){
-            res.redirect("/")
-            }
-            if(result){
-               console.log(result+"user found")
-               //bcrypt.compareSync(myPlaintextPassword, hash); //true
-                
-               if(bcrypt.compareSync(req.body.password,result.password)){
-                   if(req.session){
-                    req.session.email=result.email
-                    req.session.name=result.username
-                    res.redirect("/home")
-                    }else{
-                        res.redirect("/")
-                    }
+ 
+app.post("/",async function(req,res){
+    try {
+        req.session.email=req.body.email
+        const hash=await bcrypt.hash(req.body.password,10)
+            
+        if(req.body.btn=="register"){
+            //Creating User document
+            const User =new auth({
+                username:req.body.name,
+                email:req.body.email,
+                password:hash
+            })
+            //Save data into MongoDB
+            User.save( function(err,result){
+                if(err){
+                    res.render("login",{status:"something wrong"}) 
                 }else{
-                    res.render("login",{status:"Wrong password"})
-                }   
-           }else{
-            res.render("login",{status:"Invalid Email"})
-           }
-        })   
+                    //Here,email are also stores in file collections 
+                    const data=new imageModel({
+                        email:result.email,
+                        profile:"profileimage/woman-1807533_1920.jpg"
+                    })
+                    data.save((err,result)=>{
+                        res.render("login",{status:"Register completed"})
+                    })                
+                }  
+            })
+        }
+        if(req.body.btn=="Login"){
+     
+            auth.findOne({email:req.body.email},function(err,result){    
+             if(err){
+                 res.redirect("/")
+                 }
+                 if(result){
+                    //console.log(result+"user found")
+                    //bcrypt.compareSync(myPlaintextPassword, hash); //true
+                     
+                    if(bcrypt.compareSync(req.body.password,result.password)){
+                        if(req.session){
+                         req.session.email=result.email
+                         req.session.name=result.username
+                         res.redirect("/home")
+                         }else{
+                             res.redirect("/")
+                         }
+                     }else{
+                         res.render("login",{status:"Wrong password"})
+                     }   
+                }else{
+                 res.render("login",{status:"Invalid Email"})
+                }
+             })   
+         }   
+    } catch (error) {
+        console.log(err)
+        res.redirect("/")
     }
+    
+    
        
 })
 
